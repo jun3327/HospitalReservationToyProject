85bc6729af21d46a50d50c124585debca62b926c
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2023-09-06 22:27:08 +0900

    환자등록 일부

--- toyProject/hongikhospital/src/main/java/hongik/hongikhospital/controller/PatientController.java
+++ toyProject/hongikhospital/src/main/java/hongik/hongikhospital/controller/PatientController.java
@@ -1,2 +1,18 @@
-package hongik.hongikhospital.controller;public class PatientController {
+package hongik.hongikhospital.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class PatientController {
+
+    @GetMapping("/patients/new")
+    public String createPatientForm(Model model) {
+        model.addAttribute("patientForm", new PatientForm());
+        return "patients/createPatientForm";
+    }
 }
--- toyProject/hongikhospital/src/main/java/hongik/hongikhospital/controller/PatientForm.java
+++ toyProject/hongikhospital/src/main/java/hongik/hongikhospital/controller/PatientForm.java
@@ -1,2 +1,18 @@
-package hongik.hongikhospital.controller;public class PatientForm {
+package hongik.hongikhospital.controller;
+
+import hongik.hongikhospital.domain.Gender;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class PatientForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다")
+    private String name;
+    private int age;
+    private Gender gender;
+
 }
